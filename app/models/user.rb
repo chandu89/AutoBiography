2cca11f1f2b747eef683951c951806863367eeac
chanduwtb <chandan.choubey@tiatros.com>
2014-10-25 21:38:47 +0530

    First Name and Last name character validation and password validation is completed

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,15 +7,18 @@ class User < ActiveRecord::Base
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,:first_name,:last_name,:Mobile,:Birthday,:gender
   validates_format_of :password, :with => /[^a-zA-Z0-9]+/, :message=>"Password should be 8 characters long and should contain atleast one Uppercase, one lower case, one number, one special character", :if => :should_validate_password?
-  
+  validates_format_of :first_name, :with => /\A[a-zA-Z]+\z/, :message => "can only contain characters"
+  validates_format_of :last_name, :with => /\A[a-zA-Z]+\z/, :message => "can only contain characters"
+	
   # attr_accessible :title, :body
   has_many :autobios
-  	def Birthday
-  		d = read_attribute(:Birthday)
-  		dob = d.to_s.force_encoding("UTF-8")
-  		dob = DateTime.parse(dob).strftime("%m/%d/%Y").to_s if dob!=""
-  		dob if !dob.blank?
-	end
+ #  	def Birthday
+ #  		# d = read_attribute(:Birthday)
+ #  		# dob = d.to_date
+ #  		# dob = DateTime.parse(dob).strftime("%m/%d/%Y").to_s if dob!=""
+ #  		# dob if !dob.blank?
+ #  		Rails.logger.info("---#{self.Birthday}---")
+	# end
 	def should_validate_password?
     	 new_record?
   	end
