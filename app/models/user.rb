adb4eb8cfe3e2ff5e67d32cb89fc6a2a17b771da
chanduwtb <chandan.choubey@tiatros.com>
2014-10-28 20:29:04 +0530

    Now sign up and sign in UI changes and Password is now not restricted

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email, :password, :password_confirmation, :remember_me,:first_name,:last_name,:Mobile,:Birthday,:gender
-  validates_format_of :password, :with => /[^a-zA-Z0-9]+/, :message=>"Password should be 8 characters long and should contain atleast one Uppercase, one lower case, one number, one special character", :if => :should_validate_password?
+  # validates_format_of :password, :with => /[^a-zA-Z0-9]+/, :message=>"Password should be 8 characters long and should contain atleast one Uppercase, one lower case, one number, one special character", :if => :should_validate_password?
   validates_format_of :first_name, :with => /\A[a-zA-Z]+\z/, :message => "can only contain characters"
   validates_format_of :last_name, :with => /\A[a-zA-Z]+\z/, :message => "can only contain characters"
 	validates :Mobile, numericality: { only_integer: true,:allow_blank => true }
